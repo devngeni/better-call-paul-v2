465b8efc68bfe8c4e0bb036a5e798ebe19e09ee6
4SAMU <samuelnzomo4@gmail.com>
2024-02-26 15:52:42 +0300

    provider for getting data , by category

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ import { CartProvider } from "@/context/CartContext";
 import { useEffect, useState } from "react";
 import Script from "next/script";
 import { ServicesProvider } from "@/context/serviceContext";
+import { ServicesDataProvider } from "@/context/GetServicesDataContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [isMounted, setIsMounted] = useState<boolean>(false);
@@ -17,28 +18,30 @@ export default function App({ Component, pageProps }: AppProps) {
 
   if (!isMounted) return null;
   return (
-    <StyledComponentsRegistry>
-      <Theme>
-        <ServicesProvider>
-          <PriceProvider>
-            <CartProvider>
-              <Script
-                strategy="afterInteractive"
-                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_KEY}`}
-              />
-              <Script id="google-analytics" strategy="afterInteractive">
-                {`
+    <ServicesDataProvider>
+      <StyledComponentsRegistry>
+        <Theme>
+          <ServicesProvider>
+            <PriceProvider>
+              <CartProvider>
+                <Script
+                  strategy="afterInteractive"
+                  src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_KEY}`}
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                  {`
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
         gtag('config', '${process.env.NEXT_PUBLIC_ANALYTICS_KEY}');
     `}
-              </Script>
-              <Component {...pageProps} />
-            </CartProvider>
-          </PriceProvider>
-        </ServicesProvider>
-      </Theme>
-    </StyledComponentsRegistry>
+                </Script>
+                <Component {...pageProps} />
+              </CartProvider>
+            </PriceProvider>
+          </ServicesProvider>
+        </Theme>
+      </StyledComponentsRegistry>
+    </ServicesDataProvider>
   );
 }
