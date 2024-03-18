8b0b1a9397f4392bedc307f170ff0b5e9f12b467
Hakizimana-fred <hakifred20@gmail.com>
2024-03-18 11:24:01 +0300

    fix: build fixes

--- next.config.js
+++ next.config.js
@@ -37,7 +37,7 @@ const nextConfig = withPWA({
         return [
             {
                 
-                source: "/api/*",
+                source: "/api/:path*",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" },
