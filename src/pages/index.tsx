1131742928738412db258e724c2dfaeb415af2ee
mairura <mairurat@gmail.com>
2024-03-20 20:45:04 +0300

    updated metadata

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,18 @@ import { SplashScreen } from "@/components/splashscreen";
 import { Layout } from "@/layout";
 import BgImage from "../../public/DealsImages/bg.jpeg";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Welcome to BCP - Better Call Paul',
+  description: 'One Touch All Service from meals to wheels and beyond',
+  openGraph: {
+    images: [
+      { url: 'https://www.bettercallpaul.world/DealsImages/bgLogo.jpg', alt: 'BCP Logo' }
+    ],
+  },
+};
+
 export default function Home() {
   const router = useRouter();
 
